1822e7356f5eae2984197c252abec26ac687ec54
tychuot163 <42094955+tychuot163@users.noreply.github.com>
2021-03-18 22:05:00 +0700

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,125 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Container">
+        <div className="CardStack">
+          <div className="Card">
+            <div className="AvatarWrap">
+              <img className="Avatar" src="https://bit.ly/33ohhZH" alt="" />
+            </div>
+            <div className="Title">Facebook</div>
+            <div className="Description">
+              Connect Facebook as a social media connection for your
+              application.
+            </div>
+            <div className="Actions">
+              <a className="Link" href="/">
+                Docs
+              </a>
+              <a type="button" className="Primary" href="/">
+                Setup
+              </a>
+            </div>
+          </div>
+          <div className="Card">
+            <div className="AvatarWrap">
+              <img className="Avatar" src="https://bit.ly/2sPR8Cu" alt="" />
+            </div>
+            <div className="Title">Twitter</div>
+            <div className="Description">
+              Connect Twitter as a social media connection for your application.
+            </div>
+            <div className="Actions">
+              <a className="Link" href="/">
+                Docs
+              </a>
+              <a type="button" className="Primary" href="/">
+                Setup
+              </a>
+            </div>
+          </div>
+          <div className="Card">
+            <div className="AvatarWrap">
+              <img className="Avatar" src="https://bit.ly/2GSNm2b" alt="" />
+            </div>
+            <div className="Title">Dropbox</div>
+            <div className="Description">
+              Connect Dropbox as a social media connection for your application.
+            </div>
+            <div className="Actions">
+              <a className="Link" href="/">
+                Docs
+              </a>
+              <a type="button" className="Primary" href="/">
+                Setup
+              </a>
+            </div>
+          </div>
+          <div className="Card">
+            <div className="AvatarWrap">
+              <img className="Avatar" src="https://bit.ly/33ohhZH" alt="" />
+              <div className="StatusOn" />
+            </div>
+            <div className="Title">Facebook</div>
+            <div className="Description">
+              Connect Facebook as a social media connection for your
+              application.
+            </div>
+            <div className="Actions">
+              <a type="button" className="Secondary" href="/">
+                Test
+              </a>
+              <a type="button" className="Primary" href="/">
+                Edit
+              </a>
+            </div>
+          </div>
+          <div className="Card">
+            <div className="AvatarWrap">
+              <img className="Avatar" src="https://bit.ly/2sPR8Cu" alt="" />
+              <div className="StatusOff" />
+            </div>
+            <div className="Title">Twitter</div>
+            <div className="Description">
+              Connect Twitter as a social media connection for your application.
+            </div>
+            <div className="Actions">
+              <a type="button" className="Secondary" href="/">
+                Test
+              </a>
+              <a type="button" className="Primary" href="/">
+                Edit
+              </a>
+            </div>
+          </div>
+          <div className="Card">
+            <div className="AvatarWrap">
+              <img className="Avatar" src="https://bit.ly/2GSNm2b" alt="" />
+              <div className="StatusOn" />
+            </div>
+            <div className="Title">Dropbox</div>
+            <div className="Description">
+              Connect Dropbox as a social media connection for your application.
+            </div>
+            <div className="Actions">
+              <a type="button" className="Secondary" href="/">
+                Test
+              </a>
+              <a type="button" className="Primary" href="/">
+                Edit
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
