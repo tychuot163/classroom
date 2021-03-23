78775eb557470cf735a9035d552238ec19d7ac71
Nguyen Tu <tu.lqd.163@gmail.com>
2021-03-23 22:49:42 +0700

    css

--- src/index.tsx
+++ src/index.tsx
@@ -8,24 +8,31 @@ function App() {
     <div className="App">
       <div className="Container">
         <div className="CardStack">
-          <div className="Card">
+           <div className="Card">
             <div className="AvatarWrap">
-              <img
-                className="Avatar"
-                src="https://i.imgur.com/rkg8nKE.png"
-                alt=""
-              />
-              <div className="StatusOn" />
+            <div className="Title">           
+              BEGINNER
             </div>
-            <div className="Title">Anh văn lớp K23t</div>
+            </div>
             <div className="Description">
-              Connect Facebook as a social media connection for your
-              application.
+              Connect Twitter as a social media connection for your application.
             </div>
             <div className="Actions">
-              <a type="button" className="Secondary" href="/">
-                Test
+              <a type="button" className="Primary" href="/">
+                Edit
               </a>
+            </div>
+          </div>
+          <div className="Card">
+          <div className="AvatarWrap">
+            <div className="Title">           
+              Anh văn lớp K23t
+            </div>
+            </div>
+            <div className="Description">
+              Connect Twitter as a social media connection for your application.
+            </div>
+            <div className="Actions">
               <a type="button" className="Primary" href="/">
                 Edit
               </a>
@@ -33,21 +40,14 @@ function App() {
           </div>
           <div className="Card">
             <div className="AvatarWrap">
-              <img
-                className="Avatar"
-                src="https://i.imgur.com/rkg8nKE.png"
-                alt=""
-              />
-              <div className="StatusOff" />
+            <div className="Title">           
+              Anh văn lớp K23t
             </div>
-            <div className="Title">Anh văn lớp K23t</div>
+            </div>
             <div className="Description">
               Connect Twitter as a social media connection for your application.
             </div>
             <div className="Actions">
-              <a type="button" className="Secondary" href="/">
-                Test
-              </a>
               <a type="button" className="Primary" href="/">
                 Edit
               </a>
@@ -55,21 +55,74 @@ function App() {
           </div>
           <div className="Card">
             <div className="AvatarWrap">
-              <img
-                className="Avatar"
-                src="https://i.imgur.com/rkg8nKE.png"
-                alt=""
-              />
-              <div className="StatusOff" />
+            <div className="Title">           
+              Anh văn lớp K23t
             </div>
-            <div className="Title">Anh văn lớp K23t</div>
+            </div>
             <div className="Description">
               Connect Twitter as a social media connection for your application.
             </div>
             <div className="Actions">
-              <a type="button" className="Secondary" href="/">
-                Test
+              <a type="button" className="Primary" href="/">
+                Edit
               </a>
+            </div>
+          </div>
+          <div className="Card">
+            <div className="AvatarWrap">
+            <div className="Title">           
+              BEGINNER
+            </div>
+            </div>
+            <div className="Description">
+              Connect Twitter as a social media connection for your application.
+            </div>
+            <div className="Actions">
+              <a type="button" className="Primary" href="/">
+                Edit
+              </a>
+            </div>
+          </div>
+          <div className="Card">
+          <div className="AvatarWrap">
+            <div className="Title">           
+              Anh văn lớp K23t
+            </div>
+            </div>
+            <div className="Description">
+              Connect Twitter as a social media connection for your application.
+            </div>
+            <div className="Actions">
+              <a type="button" className="Primary" href="/">
+                Edit
+              </a>
+            </div>
+          </div>
+          <div className="Card">
+            <div className="AvatarWrap">
+            <div className="Title">           
+              Anh văn lớp K23t
+            </div>
+            </div>
+            <div className="Description">
+              Connect Twitter as a social media connection for your application.
+            </div>
+            <div className="Actions">
+              <a type="button" className="Primary" href="/">
+                Edit
+              </a>
+            </div>
+          </div>
+          <div className="Card">
+            <div className="AvatarWrap">
+            <div className="Title">           
+              Anh văn lớp K23t
+            </div>
+            </div>
+            <div className="Description">
+              Connect Twitter as a social media connection for your application.
+            </div>
+            <div className="Actions">
               <a type="button" className="Primary" href="/">
                 Edit
               </a>
